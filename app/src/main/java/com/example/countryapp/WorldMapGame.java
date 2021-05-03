7a076949cf0770b72849fb35f083acb3fbf6d63c
Hilbs100 <57808200+Hilbs100@users.noreply.github.com>
2021-05-02 20:36:08 -0400

    Switched the two home screen buttons
    I changed the text on the home screen buttons so that they would be accurate. I also added a second button just to see if we can copy and paste the FABs (Floating Action Button) and we can, but I don't know how to make them invisible.

--- app/src/main/java/com/example/countryapp/WorldMapGame.java
+++ app/src/main/java/com/example/countryapp/WorldMapGame.java
@@ -12,10 +12,11 @@ import androidx.fragment.app.Fragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class WorldMapGame extends Fragment {
+    FloatingActionButton Canada1;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.world_map_game, container, false);
-        FloatingActionButton Canada1 = (FloatingActionButton) view.findViewById(R.id.canada1);
+        Canada1 = (FloatingActionButton) view.findViewById(R.id.canada1);
         Canada1.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
