d1bee6194c6e1cad142177a34c76639709804c1c
siddhantborkar <sborkar23@rthighschool.org>
2021-05-07 13:00:32 -0400

    Fixed some stuff on the home page

---
+++ app/src/main/java/com/example/countryapp/countryMaker.java
@@ -0,0 +1,77 @@
+package com.example.countryapp;
+
+public class countryMaker {
+    public static void main (String[] args){
+        //Europe Countries added
+        GeographyGame Europe  = new GeographyGame();
+        Europe.addCountry("Russia");
+        Europe.addCountry("Germany");
+        Europe.addCountry("United Kingdom");
+        Europe.addCountry("France");
+        Europe.addCountry("Italy");
+        Europe.addCountry("Spain");
+        Europe.addCountry("Ukraine");
+        Europe.addCountry("Poland");
+        Europe.addCountry("Romania");
+        Europe.addCountry("Netherlands");
+        Europe.addCountry("Belgium");
+        Europe.addCountry("Czech Republic");
+        Europe.addCountry("Greece");
+        Europe.addCountry("Portugal");
+        Europe.addCountry("Sweden");
+        Europe.addCountry("Hungary");
+        Europe.addCountry("Belarus");
+        Europe.addCountry("Austria");
+        Europe.addCountry("Serbia");
+        Europe.addCountry("Switzerland");
+        Europe.addCountry("Bulgaria");
+        Europe.addCountry("Denmark");
+        Europe.addCountry("Finland");
+        Europe.addCountry("Slovakia");
+        Europe.addCountry("Norway");
+        Europe.addCountry("Ireland");
+        Europe.addCountry("Croatia");
+        Europe.addCountry("Moldova");
+        Europe.addCountry("Bosnia and Herzegovina");
+        Europe.addCountry("Albania");
+        Europe.addCountry("Lithuania");
+        Europe.addCountry("North Macedonia");
+        Europe.addCountry("Slovenia");
+        Europe.addCountry("Latvia");
+        Europe.addCountry("Estonia");
+        Europe.addCountry("Montenegro");
+        Europe.addCountry("Luxembourg");
+        Europe.addCountry("Malta");
+        Europe.addCountry("Iceland");
+        Europe.addCountry("Andorra");
+        Europe.addCountry("Monaco");
+        Europe.addCountry("Liechtenstein");
+        Europe.addCountry("San Marino");
+        Europe.addCountry("Holy See");
+
+        //North American Countries added
+        GeographyGame northAmerica = new GeographyGame();
+        northAmerica.addCountry("Canada");
+        northAmerica.addCountry("United States of America");
+        northAmerica.addCountry("Mexico");
+        northAmerica.addCountry("Antigua and Barbuda");
+        northAmerica.addCountry("Bahamas");
+        northAmerica.addCountry("Barbados");
+        northAmerica.addCountry("Belize");
+        northAmerica.addCountry("Costa Rica");
+        northAmerica.addCountry("Cuba");
+        northAmerica.addCountry("Dominica");
+        northAmerica.addCountry("Dominican Republic");
+        northAmerica.addCountry("El Salvador");
+        northAmerica.addCountry("Grenada");
+        northAmerica.addCountry("Guatemala");
+        northAmerica.addCountry("Haiti");
+        northAmerica.addCountry("Honduras");
+        northAmerica.addCountry("Jamaica");
+        northAmerica.addCountry("Nicaragua");
+        northAmerica.addCountry("Canada");
+        northAmerica.addCountry("Canada");
+        northAmerica.addCountry("Canada");
+
+    }
+}
