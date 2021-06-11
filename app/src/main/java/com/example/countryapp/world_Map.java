6fbfa0c398852609157fd37402c18996e3f981f6
siddhantborkar <sborkar23@rthighschool.org>
2021-06-11 13:16:47 -0400

    Game button better.

--- app/src/main/java/com/example/countryapp/world_Map.java
+++ app/src/main/java/com/example/countryapp/world_Map.java
@@ -2,6 +2,7 @@ package com.example.countryapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,11 @@ public class world_Map extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_world__map);
 
+    }
+
+    public void openHomePage(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     public void openSearchGlobe(){
