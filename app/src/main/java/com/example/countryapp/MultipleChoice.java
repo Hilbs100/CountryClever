25543859cb0273914e8570ce66708bbe96f44d2b
Hilbs100 <57808200+Hilbs100@users.noreply.github.com>
2021-09-01 12:14:33 -0400

    Back to main progress

--- app/src/main/java/com/example/countryapp/MultipleChoice.java
+++ app/src/main/java/com/example/countryapp/MultipleChoice.java
@@ -1,202 +1,222 @@
 package com.example.countryapp;
 
+import android.util.Log;
+
 public class MultipleChoice {
     // type indicates which "type" of question; i.e. capital, population, language, etc.
     private String type;
     // Let's have in between 2 and four
-     private int numAnswers;
-     private Country country;
-     private int countryIndex;
-     private String choiceA;
-     private String choiceB;
-     private String choiceC;
-     private String choiceD;
-     private String correctChoice;
-     //Constructor
-     public MultipleChoice(String type, int numAnswers, Country country) {
-         this.type = type;
-         if (numAnswers > 4) {
-             this.numAnswers = 4;
-         }
-         else if (numAnswers < 2)
-             this.numAnswers = 2;
-         else
-             this.numAnswers = numAnswers;
-         this.country = country;
-         countryIndex = Country.indexOfCountry(country);
-         createChoices();
-     }
-     /* This method creates all the choices for the Multiple Choice problem
-      All if (type.equals("blah")) contain the same basic code in them, so look at the
-      first if statement for notes */
-     private void createChoices() {
-         // instance variables
-         choiceA = "";
-         choiceB = "";
-         choiceC = "";
-         choiceD = "";
-         int rand;
-         int a;
-         int b;
-         int c;
-         int d;
-         if (type.equals("landMass")) {
-             // Uses the findOtherCountries class to find a country which hasn't been used yet
-             a = findOtherCountries(countryIndex, -1, -1, -1);
-             //Converts all choices into String form for easy usage as text later
-             choiceA = "" + (Country.getCountry(a).getLandMass());
-             b = findOtherCountries(countryIndex, a, -1, -1);
-             choiceB = "" + (Country.getCountry(b).getLandMass());
-             if (numAnswers >= 3) {
-                 c = findOtherCountries(countryIndex, a, b, -1);
-                 choiceC = "" + (Country.getCountry(c).getLandMass());
-                 if (numAnswers == 4) {
-                     d = findOtherCountries(countryIndex, a, b, c);
-                     choiceD = "" + (Country.getCountry(d).getLandMass());
-                 }
-             }
-             // This puts the choice in there as it wasn't in there before, replacing one of the
-             // ones so that the correct answer choice is there; make sure that the correctChoice is
-             // lowercase if you decide to change something
-             rand = (int) ((Math.random() * numAnswers) + 1);
-             if (rand == 1) {
-                 choiceA = "" + country.getLandMass();
-                 correctChoice = "a";
-             } else if (rand == 2) {
-                 choiceB = "" + country.getLandMass();
-                 correctChoice = "b";
-             } else if (rand == 3) {
-                 choiceC = "" + country.getLandMass();
-                 correctChoice = "c";
-             } else if (rand == 4) {
-                 choiceD = "" + country.getLandMass();
-                 correctChoice = "d";
+    private int numAnswers;
+    private Country country;
+    private int countryIndex;
+    private String choiceA;
+    private String choiceB;
+    private String choiceC;
+    private String choiceD;
+    private String correctChoice;
+    private static double score;
+    //Constructor
+    public MultipleChoice(String type, int numAnswers, Country country) {
+        this.type = type;
+        if (numAnswers > 4) {
+            this.numAnswers = 4;
+        }
+        else if (numAnswers < 2)
+            this.numAnswers = 2;
+        else
+            this.numAnswers = numAnswers;
+        this.country = country;
+        countryIndex = Country.indexOfCountry(country);
+        createChoices();
+    }
+    public void submitAnswer(String choice) {
+        Log.i("MC", "Answer submitted");
+        if (choice.equals(correctChoice)) {
+            score++;
+            Log.i("MC", "Answer was correct");
+        }
+        else
+            Log.i("MC", "Answer was incorrect");
+
+    }
+    public static void reset() {
+        score = 0;
+    }
+    public static double score() {
+        Log.i("MC", "" + 100 * (score/4.0));
+        return 100 * (score/4.0);
+    }
+    /* This method creates all the choices for the Multiple Choice problem
+     All if (type.equals("blah")) contain the same basic code in them, so look at the
+     first if statement for notes */
+    private void createChoices() {
+        // instance variables
+        choiceA = "";
+        choiceB = "";
+        choiceC = "";
+        choiceD = "";
+        int rand;
+        int a;
+        int b;
+        int c;
+        int d;
+        if (type.equals("landMass")) {
+            // Uses the findOtherCountries class to find a country which hasn't been used yet
+            a = findOtherCountries(countryIndex, -1, -1, -1);
+            //Converts all choices into String form for easy usage as text later
+            choiceA = "" + (Country.getCountry(a).getLandMass());
+            b = findOtherCountries(countryIndex, a, -1, -1);
+            choiceB = "" + (Country.getCountry(b).getLandMass());
+            if (numAnswers >= 3) {
+                c = findOtherCountries(countryIndex, a, b, -1);
+                choiceC = "" + (Country.getCountry(c).getLandMass());
+                if (numAnswers == 4) {
+                    d = findOtherCountries(countryIndex, a, b, c);
+                    choiceD = "" + (Country.getCountry(d).getLandMass());
+                }
             }
-         }
-         if (type.equals("population")) {
-             a = findOtherCountries(countryIndex, -1, -1, -1);
-             choiceA = "" + (Country.getCountry(a).getPopulation());
-             b = findOtherCountries(countryIndex, a, -1, -1);
-             choiceB = "" + (Country.getCountry(b).getPopulation());
-             if (numAnswers >= 3) {
-                 c = findOtherCountries(countryIndex, a, b, -1);
-                 choiceC = "" + (Country.getCountry(c).getPopulation());
-                 if (numAnswers == 4) {
-                     d = findOtherCountries(countryIndex, a, b, c);
-                     choiceD = "" + (Country.getCountry(d).getPopulation());
-                 }
-             }
-             rand = (int)((Math.random() * numAnswers) + 1);
-             if (rand == 1) {
-                 choiceA = "" + country.getPopulation();
-                 correctChoice = "a";
-             } else if (rand == 2) {
-                 choiceB = "" + country.getPopulation();
-                 correctChoice = "b";
-             } else if (rand == 3) {
-                 choiceC = "" + country.getPopulation();
-                 correctChoice = "c";
-             } else if (rand == 4) {
-                 choiceD = "" + country.getPopulation();
-                 correctChoice = "d";
-             }
-         }
-         if (type.equals("capital")) {
-             a = findOtherCountries(countryIndex, -1, -1, -1);
-             choiceA = "" + Country.getCountry(a).getCapital();
-             b = findOtherCountries(countryIndex, a, -1, -1);
-             choiceB = "" + (Country.getCountry(b).getCapital());
-             if (numAnswers >= 3) {
-                 c = findOtherCountries(countryIndex, a, b, -1);
-                 choiceC = "" + (Country.getCountry(c).getCapital());
-                 if (numAnswers == 4) {
-                     d = findOtherCountries(countryIndex, a, b, c);
-                     choiceD = "" + (Country.getCountry(d).getCapital());
-                 }
-             }
-             rand = (int)((Math.random() * numAnswers) + 1);
-             if (rand == 1) {
-                 choiceA = "" + country.getCapital();
-                 correctChoice = "a";
-             } else if (rand == 2) {
-                 choiceB = "" + country.getCapital();
-                 correctChoice = "b";
-             } else if (rand == 3) {
-                 choiceC = "" + country.getCapital();
-                 correctChoice = "c";
-             } else if (rand == 4) {
-                 choiceD = "" + country.getCapital();
-                 correctChoice = "d";
-             }
-         }
-         if (type.equals("language")) {
-             a = findOtherCountries(countryIndex, -1, -1, -1);
-             choiceA = "" + (Country.getCountry(a).getLanguage());
-             b = findOtherCountries(countryIndex, a, -1, -1);
-             choiceB = "" + (Country.getCountry(b).getLanguage());
-             if (numAnswers >= 3) {
-                 c = findOtherCountries(countryIndex, a, b, -1);
-                 choiceC = "" + (Country.getCountry(c).getLanguage());
-                 if (numAnswers >= 4) {
-                     d = findOtherCountries(countryIndex, a, b, c);
-                     choiceD = "" + (Country.getCountry(d).getLanguage());
-                 }
-             }
-             rand = (int)((Math.random() * numAnswers) + 1);
-             if (rand == 1) {
-                 choiceA = "" + country.getLanguage();
-                 correctChoice = "a";
-             } else if (rand == 2) {
-                 choiceB = "" + country.getLanguage();
-                 correctChoice = "b";
-             } else if (rand == 3) {
-                 choiceC = "" + country.getLanguage();
-                 correctChoice = "c";
-             } else if (rand == 4) {
-                 choiceD = "" + country.getLanguage();
-                 correctChoice = "d";
-             }
-         }
-     }
-     //finds a country of which that value has not been used; refer to createChoices
-     public int findOtherCountries(int main, int a, int b, int c) {
-         boolean run = true;
-         int rand = (int)(Math.random() * (Country.getCountryListSize()-1)) + 1;
-         while (run) {
-             if (rand != a && rand != b && rand != c && rand != main) {
-                 if (type.equals("landMass") && country.getLandMass() != Country.getCountry(rand).getLandMass()
-                         && !(choiceA.equals("" + Country.getCountry(rand).getLandMass())) &&
-                         !(choiceB.equals("" + Country.getCountry(rand).getLandMass())) &&
-                         !(choiceC.equals("" + Country.getCountry(rand).getLandMass())) &&
-                         !(choiceD.equals("" + Country.getCountry(rand).getLandMass()))) {
-                     return rand;
-                 }
-                 else if (type.equals("population") && country.getPopulation() != Country.getCountry(rand).getPopulation()
-                         && !(choiceA.equals("" + Country.getCountry(rand).getPopulation())) &&
-                         !(choiceB.equals("" + Country.getCountry(rand).getPopulation())) &&
-                         !(choiceC.equals("" + Country.getCountry(rand).getPopulation())) &&
-                         !(choiceD.equals("" + Country.getCountry(rand).getPopulation())))
-                     return rand;
-                 else if (type.equals("capital") && !(country.getCapital().equals(Country.getCountry(rand).getCapital()))
-                         && !(choiceA.equals("" + Country.getCountry(rand).getCapital())) &&
-                         !(choiceB.equals("" + Country.getCountry(rand).getCapital())) &&
-                         !(choiceC.equals("" + Country.getCountry(rand).getCapital())) &&
-                         !(choiceD.equals("" + Country.getCountry(rand).getCapital())))
-                     return rand;
-                 else if(type.equals("language") && !(country.getLanguage().equals(Country.getCountry(rand).getLanguage()))
-                         && !(choiceA.equals("" + Country.getCountry(rand).getLanguage())) &&
-                         !(choiceB.equals("" + Country.getCountry(rand).getLanguage())) &&
-                         !(choiceC.equals("" + Country.getCountry(rand).getLanguage())) &&
-                         !(choiceD.equals("" + Country.getCountry(rand).getLanguage())))
-                     return rand;
-             }
-             rand = (int)(Math.random() * (Country.getCountryListSize() - 1)) + 1;
-         }
-         return 0;
-     }
-     //Accessor Methods
+            // This puts the choice in there as it wasn't in there before, replacing one of the
+            // ones so that the correct answer choice is there; make sure that the correctChoice is
+            // lowercase if you decide to change something
+            rand = (int) ((Math.random() * numAnswers) + 1);
+            if (rand == 1) {
+                choiceA = "" + country.getLandMass();
+                correctChoice = choiceA;
+            } else if (rand == 2) {
+                choiceB = "" + country.getLandMass();
+                correctChoice = choiceB;
+            } else if (rand == 3) {
+                choiceC = "" + country.getLandMass();
+                correctChoice = choiceC;
+            } else if (rand == 4) {
+                choiceD = "" + country.getLandMass();
+                correctChoice = choiceD;
+            }
+        }
+        if (type.equals("population")) {
+            a = findOtherCountries(countryIndex, -1, -1, -1);
+            choiceA = "" + (Country.getCountry(a).getPopulation());
+            b = findOtherCountries(countryIndex, a, -1, -1);
+            choiceB = "" + (Country.getCountry(b).getPopulation());
+            if (numAnswers >= 3) {
+                c = findOtherCountries(countryIndex, a, b, -1);
+                choiceC = "" + (Country.getCountry(c).getPopulation());
+                if (numAnswers == 4) {
+                    d = findOtherCountries(countryIndex, a, b, c);
+                    choiceD = "" + (Country.getCountry(d).getPopulation());
+                }
+            }
+            rand = (int)((Math.random() * numAnswers) + 1);
+            if (rand == 1) {
+                choiceA = "" + country.getPopulation();
+                correctChoice = choiceA;
+            } else if (rand == 2) {
+                choiceB = "" + country.getPopulation();
+                correctChoice = choiceB;
+            } else if (rand == 3) {
+                choiceC = "" + country.getPopulation();
+                correctChoice = choiceC;
+            } else if (rand == 4) {
+                choiceD = "" + country.getPopulation();
+                correctChoice = choiceD;
+            }
+        }
+        if (type.equals("capital")) {
+            a = findOtherCountries(countryIndex, -1, -1, -1);
+            choiceA = "" + Country.getCountry(a).getCapital();
+            b = findOtherCountries(countryIndex, a, -1, -1);
+            choiceB = "" + (Country.getCountry(b).getCapital());
+            if (numAnswers >= 3) {
+                c = findOtherCountries(countryIndex, a, b, -1);
+                choiceC = "" + (Country.getCountry(c).getCapital());
+                if (numAnswers == 4) {
+                    d = findOtherCountries(countryIndex, a, b, c);
+                    choiceD = "" + (Country.getCountry(d).getCapital());
+                }
+            }
+            rand = (int)((Math.random() * numAnswers) + 1);
+            if (rand == 1) {
+                choiceA = "" + country.getCapital();
+                correctChoice = choiceA;
+            } else if (rand == 2) {
+                choiceB = "" + country.getCapital();
+                correctChoice = choiceB;
+            } else if (rand == 3) {
+                choiceC = "" + country.getCapital();
+                correctChoice = choiceC;
+            } else if (rand == 4) {
+                choiceD = "" + country.getCapital();
+                correctChoice = choiceD;
+            }
+        }
+        if (type.equals("language")) {
+            a = findOtherCountries(countryIndex, -1, -1, -1);
+            choiceA = "" + (Country.getCountry(a).getLanguage());
+            b = findOtherCountries(countryIndex, a, -1, -1);
+            choiceB = "" + (Country.getCountry(b).getLanguage());
+            if (numAnswers >= 3) {
+                c = findOtherCountries(countryIndex, a, b, -1);
+                choiceC = "" + (Country.getCountry(c).getLanguage());
+                if (numAnswers >= 4) {
+                    d = findOtherCountries(countryIndex, a, b, c);
+                    choiceD = "" + (Country.getCountry(d).getLanguage());
+                }
+            }
+            rand = (int)((Math.random() * numAnswers) + 1);
+            if (rand == 1) {
+                choiceA = "" + country.getLanguage();
+                correctChoice = choiceA;
+            } else if (rand == 2) {
+                choiceB = "" + country.getLanguage();
+                correctChoice = choiceB;
+            } else if (rand == 3) {
+                choiceC = "" + country.getLanguage();
+                correctChoice = choiceC;
+            } else if (rand == 4) {
+                choiceD = "" + country.getLanguage();
+                correctChoice = choiceD;
+            }
+        }
+    }
+    //finds a country of which that value has not been used; refer to createChoices
+    public int findOtherCountries(int main, int a, int b, int c) {
+        boolean run = true;
+        int rand = (int)(Math.random() * (Country.getCountryListSize()-1)) + 1;
+        while (run) {
+            if (rand != a && rand != b && rand != c && rand != main) {
+                if (type.equals("landMass") && country.getLandMass() != Country.getCountry(rand).getLandMass()
+                        && !(choiceA.equals("" + Country.getCountry(rand).getLandMass())) &&
+                        !(choiceB.equals("" + Country.getCountry(rand).getLandMass())) &&
+                        !(choiceC.equals("" + Country.getCountry(rand).getLandMass())) &&
+                        !(choiceD.equals("" + Country.getCountry(rand).getLandMass()))) {
+                    return rand;
+                }
+                else if (type.equals("population") && country.getPopulation() != Country.getCountry(rand).getPopulation()
+                        && !(choiceA.equals("" + Country.getCountry(rand).getPopulation())) &&
+                        !(choiceB.equals("" + Country.getCountry(rand).getPopulation())) &&
+                        !(choiceC.equals("" + Country.getCountry(rand).getPopulation())) &&
+                        !(choiceD.equals("" + Country.getCountry(rand).getPopulation())))
+                    return rand;
+                else if (type.equals("capital") && !(country.getCapital().equals(Country.getCountry(rand).getCapital()))
+                        && !(choiceA.equals("" + Country.getCountry(rand).getCapital())) &&
+                        !(choiceB.equals("" + Country.getCountry(rand).getCapital())) &&
+                        !(choiceC.equals("" + Country.getCountry(rand).getCapital())) &&
+                        !(choiceD.equals("" + Country.getCountry(rand).getCapital())))
+                    return rand;
+                else if(type.equals("language") && !(country.getLanguage().equals(Country.getCountry(rand).getLanguage()))
+                        && !(choiceA.equals("" + Country.getCountry(rand).getLanguage())) &&
+                        !(choiceB.equals("" + Country.getCountry(rand).getLanguage())) &&
+                        !(choiceC.equals("" + Country.getCountry(rand).getLanguage())) &&
+                        !(choiceD.equals("" + Country.getCountry(rand).getLanguage())))
+                    return rand;
+            }
+            rand = (int)(Math.random() * (Country.getCountryListSize() - 1)) + 1;
+        }
+        return 0;
+    }
+    //Accessor Methods
     public String getChoiceA() {
-         return choiceA;
+        return choiceA;
     }
     public String getChoiceB() {
         return choiceB;
